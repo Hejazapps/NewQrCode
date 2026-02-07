36b1250944eae27319bd7d20c87dc1c34f54c134
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2026-02-07 16:37:24 +0600

    tab bar install

--- NewQrMaker/Vc/HomeVc.swift
+++ NewQrMaker/Vc/HomeVc.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class HomeVc: UIViewController {
-
+    
+    @IBOutlet weak var proBtnHolder: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.applyDesignStyle(to: self.proBtnHolder)
+        }
         
-
+        
     }
     
-
-
+    func applyDesignStyle(to view: UIView) {
+        // Corner radius (fully rounded look)
+        view.layer.cornerRadius = view.frame.height / 2
+        view.layer.masksToBounds = false
+        
+        // Shadow
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.06
+        view.layer.shadowOffset = CGSize(width: 0, height: 4)
+        view.layer.shadowRadius = 2
+    }
+    
+    
+    
 }
