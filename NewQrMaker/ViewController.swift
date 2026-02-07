3cb7f70a8abee3c397f20f004e16d1f0edb68cb8
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2026-02-07 11:03:01 +0600

    all
    fixed

---
+++ NewQrMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewQrMaker
+//
+//  Created by Sadiqul AMin on 7/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
