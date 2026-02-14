3a81d478d1639493a99adc09145f7d52e749ca64
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2026-02-14 13:03:47 +0600

    libary added

---
+++ NewQrMaker/BarCodeKit/UIImage+BarCodeKit.m
@@ -0,0 +1,37 @@
+//
+//  UIImage+BarCodeKit.m
+//  BarCodeKit
+//
+//  Created by Oliver Drobnik on 8/17/13.
+//  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
+//
+
+#import "UIImage+BarCodeKit.h"
+#import "BCKCode.h"
+
+@implementation UIImage (BarCodeKit)
+
++ (UIImage *)imageWithBarCode:(BCKCode *)barCode options:(NSDictionary *)options
+{
+    NSMutableDictionary *tmpOptions = [[NSMutableDictionary alloc] initWithDictionary:options];
+    tmpOptions[BCKCodeDrawingFillEmptyQuietZonesOption] = @(YES);
+    tmpOptions[BCKCodeDrawingBarScaleOption] = [NSNumber numberWithDouble:10];
+    
+    CGSize neededSize = [barCode sizeWithRenderOptions:tmpOptions];
+
+    if (!neededSize.width || !neededSize.height)
+    {
+        return nil;
+    }
+    
+    UIGraphicsBeginImageContextWithOptions(neededSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    [barCode renderInContext:context options:tmpOptions];
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+@end
--- NewQrMaker/Vc/HomeVc.swift
+++ NewQrMaker/Vc/HomeVc.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseDatabase
 
 
+
 class HomeVc: UIViewController {
     
     @IBOutlet weak var viewAll: UILabel!
