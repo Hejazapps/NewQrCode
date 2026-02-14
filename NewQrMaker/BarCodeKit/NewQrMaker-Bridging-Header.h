5b1e36d4c7332f2c5c4a0ea69265b190a80e06a5
Rafsan Nazmul <rafsannazmul@gmail.com>
2026-02-14 19:46:13 +0600

    added BarCodeKit objC library fixing header file and settings issue fixed

---
+++ NewQrMaker/BarCodeKit/NewQrMaker-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "BarCodeKit.h"
+#import "BCKEAN13Code.h"
+#import "BCKEAN8Code.h"
+#import "BCKUPCACode.h"
+#import "BCKUPCECode.h"
+#import "UIImage+BarCodeKit.h"
