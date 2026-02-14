d793cc3504782eaae24223158c81f6c50656a837
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2026-02-14 16:43:13 +0600

    added

---
+++ NewQrMaker/BarCodeKit/NSObject+dfsfd.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+dfsfd.h
+//  NewQrMaker
+//
+//  Created by Sadiqul AMin on 14/2/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (dfsfd)
+
+@end
+
+NS_ASSUME_NONNULL_END
